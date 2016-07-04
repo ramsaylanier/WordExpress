442a492563467c804c12796000f79ea26ca71e41
Ramsay Lanier <ramsay@nclud.com>
2016-07-04 12:29:36 -0400

    formatting FPL

--- app/components/layouts/FrontPageLayout.js
+++ app/components/layouts/FrontPageLayout.js
@@ -4,9 +4,7 @@ import PostContent from '../posts/PostContent';
 import { connect } from 'react-apollo';
 
 class FrontPageLayout extends React.Component{
-
-	render(){
-    console.log('front page render');
+  render(){
     const { loading } = this.props.page;
 
     if (loading){
@@ -15,26 +13,26 @@ class FrontPageLayout extends React.Component{
       )
     } else {
       const { post_title, post_content, thumbnail} = this.props.page.page;
-  		let bg = {
-  			backgroundImage: "url('" + thumbnail + "')"
-  		}
+      let bg = {
+        backgroundImage: "url('" + thumbnail + "')"
+      }
 
-  		let heroClass = thumbnail ? "hero_thumbnail" : "hero"
+      let heroClass = thumbnail ? "hero_thumbnail" : "hero"
 
-  		return (
-  			<div>
-  				<div styleName={heroClass} style={bg}>
-  					<div styleName="wrapper tight">
-  						<h1 styleName="title">WordExpress</h1>
-  						<h4 styleName="subtitle">WordPress using Node, Express, and React.</h4>
-  					</div>
-  				</div>
+      return (
+        <div>
+          <div styleName={heroClass} style={bg}>
+            <div styleName="wrapper tight">
+              <h1 styleName="title">WordExpress</h1>
+              <h4 styleName="subtitle">WordPress using Node, Express, and React.</h4>
+            </div>
+          </div>
 
-  				<div styleName="content">
-  					<div styleName="wrapper tight">
-  						<PostContent post_content={post_content}/>
-  					</div>
-  				</div>
+          <div styleName="content">
+            <div styleName="wrapper tight">
+              <PostContent post_content={post_content}/>
+            </div>
+          </div>
         </div>
   		)
     }
