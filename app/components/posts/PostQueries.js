da000771aca0280873bf3ca3d784af0df805a855
Ramsay Lanier <ramsay@nclud.com>
2016-07-13 10:29:33 -0400

    separate out PostList queries for reusability

---
+++ app/components/posts/PostQueries.js
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag';
+
+export function PostListQuery({ownProps, state}){
+  return {
+    getPosts: {
+      query: gql`
+        query getPosts($post_type: String){
+          posts(post_type: $post_type){
+            id
+            post_title
+            post_name
+            post_content
+            post_parent
+          }
+        }
+      `,
+      variables: {
+        post_type: ownProps.layout.postType
+      }
+    }
+  }
+}
+
+export function PostQuery({ownProps, state}){
+  return {
+    getPost: {
+      query: gql`
+        query post($id: Int){
+          post(id: $id){
+            id
+            post_title
+            post_name
+            post_content
+            post_parent
+          }
+        }
+      `,
+      variables: {
+        id: ownProps.id
+      }
+    }
+  }
+}
