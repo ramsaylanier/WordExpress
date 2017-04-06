0418ab9c642ce57f4e3b66fbd5a1cf3ba3b6a8c3
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-04-06 12:59:51 -0400

    formatting

--- app/filters/Posts/FiltersPostsWithChildren.js
+++ app/filters/Posts/FiltersPostsWithChildren.js
@@ -1,35 +1,35 @@
-import {filter, map, remove} from 'lodash';
+import {filter, map, remove} from 'lodash'
 
 // takes unsorted posts and reduces them to top levels posts with a nested array of children
 export default function FilterPostsWithChildren(posts) {
   function getChildren(post) {
-    const { id } = post;
+    const { id } = post
     const children = filter(posts, childPost => {
-      return childPost.post_parent === id;
-    });
+      return childPost.post_parent === id
+    })
 
     if (children.length > 0) {
       map(children, child => {
-        const childWithChildren = getChildren(child);
+        const childWithChildren = getChildren(child)
         if (!post.children) {
-          post.children = [];
+          post.children = []
         }
 
-        post.children.push(childWithChildren);
-      });
+        post.children.push(childWithChildren)
+      })
     }
 
-    return post;
+    return post
   }
 
   const topLevelPosts = remove( posts, post => {
-    return post.post_parent === 0;
-  });
+    return post.post_parent === 0
+  })
 
   map(topLevelPosts, topLevelPost => {
-    topLevelPost.children = [];
-    return getChildren(topLevelPost);
-  });
+    topLevelPost.children = []
+    return getChildren(topLevelPost)
+  })
 
-  return topLevelPosts;
+  return topLevelPosts
 }
