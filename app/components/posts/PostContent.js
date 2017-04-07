9c34433749d68bcff91cb1043172aeb63b3006f7
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-04-07 10:23:05 -0400

    add sytax highlight to code and pre nodes

--- app/components/posts/PostContent.js
+++ app/components/posts/PostContent.js
@@ -5,9 +5,15 @@ import CSSModules from 'react-css-modules'
 import styles from './post.scss'
 import Shortcodes  from '../shortcodes/shortcodes'
 import {TweenMax, Power4} from 'gsap'
+import hljs from 'highlightjs'
 
 @CSSModules(styles, {allowMultiple: true})
 class PostContent extends Component {
+
+  componentDidUpdate() {
+
+  }
+
   componentDidMount() {
     const content = this._content
     const anchors = content.getElementsByTagName('a')
@@ -32,8 +38,14 @@ class PostContent extends Component {
     })
 
     // render embed shortcodes
-    const shortcodes = document.getElementsByClassName('post--shortcode')
+    const shortcodes = document.getElementsByClassName('post__shortcode')
     map(shortcodes, (shortcode) => Shortcodes.renderEmbed(shortcode))
+
+    // syntax highlight for code and pre elements
+    const code = document.getElementsByTagName('pre')
+    for (let i = 0; i < code.length; i++) {
+      hljs.highlightBlock(code[i])
+    }
   }
 
   _parseContent() {
@@ -42,10 +54,10 @@ class PostContent extends Component {
     const splitContent = trimmed.split('\n')
     const voidTags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'code', 'pre', 'img']
     const shortcodes = ['caption', 'embed', 'gist']
+    let tagIsClosed = true
 
     map(splitContent, (line, index) => {
       let newline = line
-
       // check if line is a shortcode
       if (newline[0] === '[') {
         const shortcode = line.match(/([[])\w+/g).toString().substr(1)
@@ -56,6 +68,7 @@ class PostContent extends Component {
         // wrap lines without voidTags in paragraph tags
         let tag = line.match(/^<\w+/g)
         tag = tag ? tag[0].slice(1) : ''
+        tagIsClosed = line.match(/<\/\w*>$/g)
         if (voidTags.indexOf(tag) === -1 && line.length > 1) {
           newline = '<p>' + line + '</p>'
         }
